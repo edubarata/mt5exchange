9fc21834f1f0cc8639bcae783dca828e7ce7073c
Eduardo Barata <80075483+edubarata@users.noreply.github.com>
2024-12-26 10:55:19 -0300

    Create mt5exchange.py

---
+++ mt5exchange.py
@@ -0,0 +1,311 @@
+import MetaTrader5 as mt5
+import pandas as pd
+#from binance.client import Client
+#from binance.enums import *
+import binance
+from datetime import datetime,timedelta
+
+class MTrader():
+    def __init__(self,servidor,user_login,senha,verbose=True):
+        if verbose:
+            print("Estabelecendo Conexão com Metatrader 5:")
+            print("  Server: "+servidor)
+            print("  Login:  "+str(user_login))
+        self.client = mt5.initialize(login=user_login,server=servidor,password=senha)
+        if not self.client:
+            print("Initialize() failed, error code =",mt5.last_error())
+            self.error = True
+            quit()
+        else:
+            self.error = False
+
+    def read_price_day(self,papel):
+        rates = mt5.copy_rates_from_pos(papel, mt5.TIMEFRAME_D1, 0, 2)
+        fechamento_anterior = rates[0][4]
+        valor_atual = rates[1][4]
+        abertura_atual = rates[1][1]
+        return fechamento_anterior,abertura_atual,valor_atual
+
+    def read_info(self,papel):
+        symbol_info=mt5.symbol_info(papel)
+
+        if symbol_info!=None:
+            return symbol_info.last,symbol_info.bid,symbol_info.ask
+        else:
+            return 0,0,0
+
+    def read_positions(self,ativo):
+        position = Position()
+        aux = mt5.positions_get(symbol=ativo)
+        for i in range(len(aux)):
+            position.ticket          = aux[0][0]
+            position.time            = str(pd.to_datetime(aux[0][1],unit='s'))[11:]
+            position.time_msc        = pd.to_datetime(aux[0][2],unit='ms')
+            position.time_update     = pd.to_datetime(aux[0][3],unit='ms')
+            position.time_update_msc = pd.to_datetime(aux[0][4],unit='ms')
+            position.type            = aux[0][5]
+            position.magic           = aux[0][6]
+            position.identifier      = aux[0][7]
+            position.reason          = aux[0][8]
+            position.volume          = aux[0][9]
+            position.price_open      = aux[0][10]
+            position.sl              = aux[0][11]
+            position.tp              = aux[0][12]
+            position.price_current   = aux[0][13]
+            position.swap            = aux[0][14]
+            position.profit          = aux[0][15]
+            position.symbol          = aux[0][16]
+            position.comment         = aux[0][17]
+            position.external_id     = aux[0][18]
+        return position
+
+    def read_position(self,ativo):
+        #print('ativo:',ativo)
+        info_posicoes = mt5.positions_get(symbol=ativo)
+        try:
+            if len(info_posicoes) == 0:
+                df = pd.DataFrame()
+                return df
+        except:
+            print('Programa Terminado por erro')
+            df = pd.DataFrame()
+            return df
+        #print('info_posicoes-123 - len=',len(info_posicoes))
+        #print(info_posicoes)
+        
+        
+        df = pd.DataFrame(list(info_posicoes),columns=info_posicoes[0]._asdict().keys())
+        df ['time'] = pd.to_datetime (df ['time'], unit='s')
+        return df
+
+        aux = mt5.positions_get(symbol=ativo)
+        #print('aux')
+        #print(aux)
+        position = pd.DataFrame(aux)
+        #print('position')
+        #print(position)
+        return position
+
+    def verify_position(self,ativo,position_verify):
+        position = Position()
+        aux = mt5.positions_get(symbol=ativo)
+        #print('ddd-1')
+        #print(pd.DataFrame(aux))
+        #print('ddd-2')
+        if len(aux) >= 1:
+            return True,aux[0].price_open,aux[0].sl,aux[0].tp
+        return False,0,0,0
+
+    def read_order(self,ativo):
+        order = Order()
+        aux = mt5.orders_get(symbol=ativo)
+        print(aux)
+        for i in range(len(aux)):
+            order.len             = aux[0][0]
+            order.status          = str(pd.to_datetime(aux[0][1],unit='s'))[11:]
+            order.boleta          = pd.to_datetime(aux[0][2],unit='ms')
+            order.time_setup      = pd.to_datetime(aux[0][3],unit='ms')
+            order.time_setup_msc  = pd.to_datetime(aux[0][4],unit='ms')
+            order.time_expiration = aux[0][5]
+            order.type            = aux[0][6]
+            order.type_time       = aux[0][7]
+            order.type_filling    = aux[0][8]
+            order.state           = aux[0][9]
+            order.magic           = aux[0][10]
+            order.volume_current  = aux[0][11]
+            order.price_open      = aux[0][12]
+            order.sl              = aux[0][13]
+            order.tp              = aux[0][14]
+            order.price_current   = aux[0][15]
+            order.symbol          = aux[0][16]
+        return order
+
+    def verify_order(self,ativo,order_verify):
+        order = Order()
+        aux = mt5.orders_get(symbol=ativo)
+        for i in range(len(aux)):
+            if order_verify == aux[i].ticket:
+                return True,aux[i].price_open,aux[i].sl,aux[i].tp
+        return False,0,0,0
+
+    def read_candles_mt5(self,symbol,tf,n=1,time_index=False):
+        # symbol = 'PETR4'
+        # tf = '1m' / '5m' / '1h' / '1d' / ...
+        # n = 0 (opened candle)
+        # n = 1 (last closed candle)
+        # n = 2.. (las 2.. closed candles)
+        dictionary = {
+            '1m':  mt5.TIMEFRAME_M1,
+            '2m':  mt5.TIMEFRAME_M2,
+            '5m':  mt5.TIMEFRAME_M5,
+            '15m': mt5.TIMEFRAME_M15,
+            '30m': mt5.TIMEFRAME_M30,
+            '1h':  mt5.TIMEFRAME_H1,
+            '1d':  mt5.TIMEFRAME_D1
+        }
+        n = n + 1
+        timef = dictionary[tf]
+        df = pd.DataFrame(mt5.copy_rates_from_pos(symbol, timef, 0, n))
+        df = df.rename({'real_volume': 'volume'}, axis=1)
+        #print('antes do erro 2:',symbol,timef,n)
+        #print(df)
+        df['volume'] = df['volume'].astype(float)
+        df['tick_volume'] = df['tick_volume'].astype(float)
+        df['time'] = pd.to_datetime(df['time'],unit='s')
+        df.drop(["spread"], axis=1,inplace=True)
+        return df
+
+    def read_candles(self,symbol,tf,n=0,verbose=False):
+        # Faz a leitura de "n" candles de "symbol" no timeframe "tf"
+        # Se n == 0 devolve o candle atual ainda nao fechado
+
+        candles = mt5.get_klines(symbol=symbol, interval=tf, limit=1)
+        if n==0:
+            df = pd.DataFrame(candles)
+            df.columns = ['time','open','high','low','close','volume','a','b','c','d','e','f']
+            df.drop(['a','b','c','d','e','f'],axis=1,inplace=True)
+            df['time'] = pd.to_datetime(df['time'], unit='ms')
+            df.set_index('time',inplace=True)
+            return df
+        if   tf == '1m':  delta =    60000
+        elif tf == '5m':  delta =   300000
+        elif tf == '15m': delta =   900000
+        elif tf == '1h':  delta =  3600000
+        elif tf == '1d':  delta = 86400000
+        else:
+            print('tf error')
+            quit()
+        t_atual = candles[0][0]
+        df = pd.DataFrame()
+        while n > 0:
+            if verbose:
+                print('Lendo histórico. n = {}   '.format(n),end='\r')
+            if n > 500:
+                tini = t_atual - n * delta
+                n_candles = 500
+                n = n - 500
+            else:
+                tini = t_atual - n * delta
+                n_candles = n
+                n = 0
+            candles = self.client.get_klines(symbol=symbol, interval=tf, limit=n_candles, startTime=tini)
+            c = pd.DataFrame(candles)
+            c.columns = ['time','open','high','low','close','volume','a','b','c','d','e','f']
+            c.drop(['a','b','c','d','e','f'],axis=1,inplace=True)
+            c['time'] = pd.to_datetime(c['time'], unit='ms')
+            df = pd.concat([df, c])
+        if verbose:
+            print(50*' ')
+        df = df.astype({"open": float, "high": float, "low": float, "close": float, "volume": float})
+        df.reset_index(drop=True,inplace=True)
+        return(df)
+
+    def read_OHLC(self,symbol,tf,n=1,time_index=False):
+        # symbol = 'PETR4'
+        # tf = '1m' / '5m' / '1h' / '1d' / ...
+        # n = 0 (last open candle)
+        # n = 1 (last closed candle)
+        # n = 2.. (las 2.. closed candles)
+        dictionary = {
+            '1m':mt5.TIMEFRAME_M1,
+            '5m':mt5.TIMEFRAME_M5,
+            '1h':mt5.TIMEFRAME_H1,
+            '1d':mt5.TIMEFRAME_D1
+        }
+        n = n + 1
+        timef = dictionary[tf]
+        df = pd.DataFrame(mt5.copy_rates_from_pos(symbol, timef, 0, n))
+        df = df.rename({'real_volume': 'volume'}, axis=1)
+        df['volume'] = df['volume'].astype(float)
+        df['time'] = pd.to_datetime(df['time'],unit='s')
+        df.drop(["tick_volume"], axis=1,inplace=True)
+        df.drop(["spread"], axis=1,inplace=True)
+        return df
+
+    def create_df_file(symbol,tf,n,filename):
+        df = read_candles_mt5(symbol,tf,n)
+        df.to_csv(filename)
+
+    def get_book(self,ativo,data_inicio,n=1000):
+        '''
+        TICK_FLAG define possíveis sinalizadores para ticks. Esses sinalizadores são usados para descrever os ticks recebidos pelas funções copy_ticks_from() e copy_ticks_range().
+
+        Identificador         Descrição
+
+        TICK_FLAG_BID         preço Bid alterado
+        TICK_FLAG_ASK         preço Ask alterado
+        TICK_FLAG_LAST        preço Last alterado
+        TICK_FLAG_VOLUME      volume alterado (Volume)
+        TICK_FLAG_BUY         preço da última compra alterado (Buy)
+        TICK_FLAG_SELL        preço da última venda alterado (Sell)
+        '''
+        print(mt5.TICK_FLAG_BID,'preço Bid alterado')
+        print(mt5.TICK_FLAG_ASK,'preço Ask alterado')
+        print(mt5.TICK_FLAG_LAST,'preço Last alterado')
+        print(mt5.TICK_FLAG_VOLUME,'volume alterado (Volume)')
+        print(mt5.TICK_FLAG_BUY,'preço da última compra alterado (Buy)')
+        print(mt5.TICK_FLAG_SELL,'preço da última venda alterado (Sell)')
+
+        ativo = mt5.copy_ticks_from(ativo,data_inicio,n,mt5.COPY_TICKS_ALL)
+        ativo = pd.DataFrame(ativo)
+        print('ativo111')
+        print(ativo)
+        ativo['time']=pd.to_datetime(ativo['time'],unit='s')
+        return ativo
+
+class Tick:
+    def __init__(self):
+        self.time        = ''
+        self.bid         = 0.0
+        self.bidqty      = 0.0
+        self.ask         = 0.0
+        self.askqty      = 0.0
+        self.last        = 0.0
+        self.volume      = 0.0
+        self.time_msc    = ''
+        self.flags       = 0
+        self.volume_real = 0.0
+
+class Position:
+    def __init__(self):
+        self.len             = 0
+        self.ticket          = 0
+        self.time            = ''
+        self.time_msc        = ''
+        self.time_update     = ''
+        self.time_update_msc = ''
+        self.type            = 0
+        self.magic           = 0
+        self.identifier      = 0
+        self.reason          = 0
+        self.volume          = 0.0
+        self.price_open      = 0.0
+        self.sl              = 0.0
+        self.tp              = 0.0
+        self.price_current   = 0.0
+        self.swap            = 0
+        self.profit          = 0
+        self.symbol          = ''
+        self.comment         = ''
+        self.external_id     = 0
+
+class Order:
+    def __init__(self):
+        self.len             = 0
+        self.status          = False
+        self.boleta          = 0          # ticket
+        self.time_setup      = 0          # time_setup
+        self.time_setup_msc  = 0          # time_setup_msc
+        self.time_expiration = 0          # time_expiration
+        self.type            = 0          # type
+        self.type_time       = 0          # type_time
+        self.type_filling    = 0          # type_filling
+        self.state           = 0          # state
+        self.magic           = 0          # magic
+        self.volume_current  = 0.0        # volume_current
+        self.price_open      = 0.0        # price_open
+        self.sl              = 0.0        # sl
+        self.tp              = 0.0        # tp
+        self.price_current   = 0.0        # price_current
+        self.symbol          = ''         # symbol
+
