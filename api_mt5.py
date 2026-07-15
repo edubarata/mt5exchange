88feb13aa5069ae113cd251350efe3a55e05973f
edubarata <eduardopcastro@gmail.com>
2026-07-15 09:53:55 -0300

    20260715-1

--- api_mt5.py
+++ api_mt5.py
@@ -221,6 +221,7 @@ def order():
     price    = float(request.args.get("price"))
     symb_sel_result = conn.symbol_select(symbol)
     order_result    = conn.order(buy_sell, symbol, volume, price)
+    print(f"order_result:: |{order_result}|")
     return jsonify({
         "symb_sel_result" : symb_sel_result,
         "order_result"    : order_result
